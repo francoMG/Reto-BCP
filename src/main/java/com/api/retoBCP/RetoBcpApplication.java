3739572315f918027434cb35b338fb811333f5b5
arian100y <arian1234y@gmail.com>
2020-09-17 16:34:41 -0500

    Added Swagger

--- src/main/java/com/api/retoBCP/RetoBcpApplication.java
+++ src/main/java/com/api/retoBCP/RetoBcpApplication.java
@@ -2,6 +2,8 @@ package com.api.retoBCP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class RetoBcpApplication {
