7a98818718ea478e5299a2460c98f0a6dd806488
arian100y <arian1234y@gmail.com>
2020-09-17 17:24:09 -0500

    Added generated ID's

--- src/main/java/com/api/retoBCP/model/Notification.java
+++ src/main/java/com/api/retoBCP/model/Notification.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 public class Notification {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Integer user_id;
     private String title;
